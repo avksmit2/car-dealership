726e1aa7fa1cdffc79f6ce2e11ca6dd136df9898
Angie Smith and Josh Huffman <josh@>
2016-09-08 10:02:57 -0700

    add ability to create new listing

--- app/app.php
+++ app/app.php
@@ -6,7 +6,17 @@
 
     if (empty($_SESSION['cars'])) {
         $_SESSION['cars'] = array();
+        $porsche = new Car("2014 Porsche 911", 114991, 7864, "img/porsche911.jpg");
+
+        $ford = new Car("2011 Ford F450", 55995, 14241, "img/fordf450.jpg");
+
+        $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "img/lexus350.jpg");
+
+        $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "img/mercedescls550.jpg");
+        $_SESSION['cars'] = array($porsche, $ford, $lexus, $mercedes);
     }
+
+
 
     $app = new Silex\Application();
 
@@ -15,71 +25,25 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('search_car.html.twig');
-        // "<!DOCTYPE html>
-        // <html>
-        // <head>
-        //     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-        //     <title>Find a Car</title>
-        // </head>
-        // <body>
-        //     <div class='container'>
-        //         <h1>Find a Car!</h1>
-        //         <form action='/car'>
-        //             <div class='form-group'>
-        //                 <label for='price'>Enter Maximum Price:</label>
-        //                 <input id='price' name='price' class='form-control' type='number'>
-        //             </div>
-        //             <div class='form-group'>
-        //               <label for='mileage'>Enter Maximum Mileage:</label>
-        //               <input id='mileage' name='mileage' class='form-control' type='number'>
-        //             <button type='submit' class='btn-success'>Submit</button>
-        //         </form>
-        //     </div>
-        // </body>
-        // </html>";
     });
 
     $app->post('/car', function() use ($app) {
-        $porsche = new Car("2014 Porsche 911", 114991, 7864, "img/porsche911.jpg");
 
-        $ford = new Car("2011 Ford F450", 55995, 14241, "img/fordf450.jpg");
-
-        $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "img/lexus350.jpg");
-
-        $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "img/mercedescls550.jpg");
-        $cars = array($porsche, $ford, $lexus, $mercedes);
         $cars_matching_search = array();
 
-        foreach ($cars as $car) {
+        foreach ($_SESSION['cars'] as $car) {
             if ($car->worthBuying($_POST['price'], $_POST['mileage'])) {
                 array_push($cars_matching_search, $car);
             }
         }
-        // $randomName = "<!DOCTYPE html>
-        // <html>
-        // <head>
-        //     <title>Your Car Dealership's Homepage</title>
-        // </head>
-        // <body>
-        //     <h1>Your Car Dealership</h1>
-        //     <ul>";
-        // if (empty($cars_matching_search)) {
-        //     $randomName = $randomName."Enter a higher number you cheap bastard.";
-        // }
-        // foreach ($cars_matching_search as $car) {
-        //
-        //     $randomName = $randomName."<li>".$car->getMakeModel()."</li>".
-        //     "<ul>".
-        //         '<li> $'.$car->getPrice().'</li>'.
-        //         '<li> Miles: '.$car->getMiles().'</li>'.
-        //         "<li> <img src='".$car->getPicture()."'                     > </li>".
-        //     '</ul>';
-        // }
-        // $randomName = $randomName."</ul>
-        // </body>
-        // </html>";
 
         return $app['twig']->render('cars.html.twig', array('cars' => $cars_matching_search));
+    });
+
+    $app->post('/addcar', function() use($app) {
+        $newCar = new Car($_POST['make'], $_POST['newPrice'], $_POST['newMiles'], $_POST['picture']);
+        $newCar->save();
+        return $app['twig']->render('add_car.html.twig', array('newcar' => $newCar));
     });
 
     return $app;
